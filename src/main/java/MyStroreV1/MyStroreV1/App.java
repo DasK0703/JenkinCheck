a2122c1c4d07f54f6b464668bec34cc4c936cd36
DasK0703 <daskishor0703@gmail.com>
2025-09-04 14:13:10 +0530

    add only

---
+++ src/main/java/MyStroreV1/MyStroreV1/App.java
@@ -0,0 +1,13 @@
+package MyStroreV1.MyStroreV1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
